5b670ae818c0eca1f5843b631c5973e5171f7c3d
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2019-05-15 19:39:15 +0200

    Start

--- Pobieranie/Form1.Designer.cs
+++ Pobieranie/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(16, 35);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(358, 22);
+            this.adres.TabIndex = 0;
+            this.adres.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 16;
+            this.lista.Items.AddRange(new object[] {
+            "openload"});
+            this.lista.Location = new System.Drawing.Point(388, 38);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(182, 20);
+            this.lista.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(598, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "pobranie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Pobieranie/Form1.cs
+++ Pobieranie/Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.XPath;
+using HtmlAgilityPack;
 
 namespace Pobieranie
 {
@@ -15,6 +18,49 @@ namespace Pobieranie
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    progressBar.Value = e.ProgressPercentage;
+        //}
+        //private void click_Click(object sender, EventArgs e)
+        //{
+        //    WebClient webClient = new WebClient();
+        //    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompleted);
+        //    webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+        //    string sourceFile = $"http://openload.co/stream/Ki5y8-mPcoE~1558022792~95.108.0.0~mdmeUiAb?";
+        //    webClient.DownloadFileAsync(new Uri(sourceFile), "test.mp4");
+        //}
+        //private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        //{
+        //    MessageBox.Show("The download is completed!");
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                string page = adres.Text;
+                string html = client.DownloadString(page);
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+
+                
+
+                HtmlNodeNavigator navigator = (HtmlNodeNavigator)doc.CreateNavigator();
+                string xPath = "//*[@id=\"olvideo_html5_api\"]";
+
+                string val = navigator.SelectSingleNode(xPath).Value;
+                adres.Text = val;
+            }
         }
     }
 }
